9a7ed0222165147da4c0ff7cfe38b9b3e0661937
Jakob A <jdanders1999@gmail.com>
2023-01-11 16:10:27 -0500

    Added/updated card for Manager class html

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -45,3 +45,22 @@ function cardEngineer(employee) {
     </div>
 </div>`
 }; 
+
+function cardManager(employee) {
+    return `<div class="card" style="width: 16rem; border-radius: 0;>
+    <section style="background-color: gray; color: white; padding: 5px 8px; margin: 0; height: 75px;">
+        <p style="margin: 2px 5px;">${employee.name}</p>
+        <p style="margin: 0 0 5px;"><i class="bi-cup-hot-fill"></i>${employee.userRole()}<p>
+    </section>
+    <div class="card-body bg-light" style="padding: 30px 15px;">
+        <div class="d-block" style="margin: 0;">
+            <p style="height: 55px; background-color: white; border:solid 1px blue; margin: 0; padding; 10px 5px;>
+                ID: ${employee.id}</p>
+            <p style="height: 55px; background-color: white; border:solid 1px blue; margin: 0; padding; 10px 5px;>
+                Email: ${employee.email}</p>
+            <p style="height: 55px; background-color: white; border:solid 1px blue; margin: 0; padding; 10px 5px;>
+                Office Number: ${employee.ofcNum}</p>
+        </div>
+    </div>
+</div>`
+}; 
