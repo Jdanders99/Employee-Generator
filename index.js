6e799cccb10ad42f0a1d2a3e057d3011581a7fe4
Jakob A <jdanders1999@gmail.com>
2023-01-11 16:32:49 -0500

    Added files required in index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const Employee = require('./lib/employee.js');
+const Engineer = require ('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const manager = require('./lib/manager.js');
+const inquirer = require ('inquirer');
+const fs = reqiure('fs');
+const generateHTML = require('./utils/generateHTML.js');
+const team = [];
 
 
